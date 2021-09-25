c8b26f4929dd1216f5a4661af19b6ede44d42679
Carlos Gomez <carlosalbertogomez108@gmail.com>
2021-09-24 20:46:09 -0600

    Create fetch funtions on apiCalls file

---
+++ src/apiCalls.js
@@ -0,0 +1,30 @@
+/* eslint-disable max-len */
+function fetchData(file) {
+  return fetch(`http://localhost:3001/api/v1/${file}`)
+    .then(response => response.json());
+}
+
+function updateBookings(userID, date, roomNumber) {
+  fetch('http://localhost:3001/api/v1/bookings', {
+    method: 'POST',
+    body: JSON.stringify({
+      userID,
+      date,
+      roomNumber,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => checkResponse(response))
+    .catch(error => console.warn(error));
+}
+
+function checkResponse(response) {
+  if (!response.ok) {
+    throw new Error(`Status: ${response.status} StatusText: ${response.status.text}`);
+  }
+  return response.json();
+}
+
+export { fetchData, updateBookings };
