eaadead04ea19e62c3f2ce02c2143ba81fafd030
Carlos Gomez <carlosalbertogomez108@gmail.com>
2021-09-24 19:59:02 -0600

    Add method to booking class

--- src/classes/booking.js
+++ src/classes/booking.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 class Booking {
   constructor(userID, checkInDate, checkOutDate, roomNumber) {
     this.userID = userID;
@@ -5,6 +6,7 @@ class Booking {
     this.checkOut = checkOutDate;
     this.roomNumber = roomNumber;
     this.singleBookings = [];
+    this.bookingCost = 0;
   }
 
   getSingleBookings(hotel) {
@@ -19,6 +21,13 @@ class Booking {
     })
     this.singleBookings = bookingsToPost;
     return bookingsToPost;
+  }
+
+  getBookingCost(hotel) {
+    const room = hotel.roomsToDisplay.find(room => room.number === this.roomNumber);
+    const bookingCost = room.costPerNight * this.singleBookings.length;
+    this.bookingCost = bookingCost;
+    return bookingCost;
   }
 }
 
